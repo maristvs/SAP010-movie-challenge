a4a19064ac61151c8400b156e8640f4fa9b6c87a
maristvs <mariannalves5zoo@gmail.com>
2023-09-18 20:16:14 -0300

    trocando a api omdb para a tmdb para usar melhor as funcionalidades disponíveis

--- src/request.js
+++ src/request.js
@@ -1,44 +1,72 @@
-const apiKey = "1e75eb9f";
-const movieContainer = document.getElementById("movie-container");
+document.addEventListener("DOMContentLoaded", () => {
+  const apiKey = "21a573ad60a6315aa3f1df90fe857dc4"; // Substitua pelo seu API key da TMDB
+  const movieContainer = document.getElementById("movie-container");
+  const popularLink = document.getElementById("popular");
+  const actionLink = document.getElementById("action");
+  const comedyLink = document.getElementById("comedy");
+  const animationLink = document.getElementById("animation");
 
-    async function fetchMovies() {
-      try {
-        const type = "movie"; // Para buscar filmes populares
-        const response = await fetch(
-          `https://www.omdbapi.com/?apikey=${apiKey}&s=${type}`
-        );
+  // Função para buscar filmes com base na categoria
+  async function fetchMovies(category) {
+    try {
+      // Mapeie as categorias para os IDs da TMDB (você pode adicionar mais mapeamentos conforme necessário)
+      const categoryMap = {
+        "Filmes Populares": "popular",
+        Ação: "28",
+        Comédia: "35",
+        Animação: "16",
+      };
 
-        if (response.ok) {
-          const data = await response.json();
+      const categoryId = categoryMap[category];
+      if (!categoryId) {
+        console.log("Categoria não mapeada.");
+        return;
+      }
 
-          if (data.Search) {
-            const movies = data.Search;
-            movieContainer.innerHTML = "";
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${categoryId}`
+      );
 
-            for (const movie of movies) {
-              const title = movie.Title;
-              const posterPath = movie.Poster;
-              const releaseYear = movie.Year;
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
 
-              const movieCard = document.createElement("div");
-              movieCard.classList.add("movie-card");
-              movieCard.innerHTML = `
-                <img src="${posterPath}" alt="${title}">
-                <h2>${title}</h2>
-                <p>Lançamento: ${releaseYear}</p>
-              `;
+        if (data.results) {
+          const movies = data.results;
+          movieContainer.innerHTML = "";
 
-              movieContainer.appendChild(movieCard);
-            }
-          } else {
-            console.log("Nenhum filme encontrado.");
+          for (const movie of movies) {
+            const title = movie.title;
+            const posterPath = movie.poster_path;
+            const releaseYear = movie.release_date ? new Date(movie.release_date).getFullYear() : "";
+
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("movie-card");
+            movieCard.innerHTML = `
+              <img src="https://image.tmdb.org/t/p/w500${posterPath}" alt="${title}">
+              <h2>${title}</h2>
+              <p>Lançamento: ${releaseYear}</p>
+            `;
+
+            movieContainer.appendChild(movieCard);
           }
         } else {
-          console.log(response.status);
+          console.log("Nenhum filme encontrado.");
         }
-      } catch (error) {
-        console.error("Erro ao buscar filmes:", error);
+      } else {
+        console.log(response.status);
       }
+    } catch (error) {
+      console.error("Erro ao buscar filmes:", error);
     }
+  }
 
-    fetchMovies();
+  // Adicione manipuladores de eventos de clique para as categorias
+  popularLink.addEventListener("click", () => fetchMovies("Filmes Populares"));
+  actionLink.addEventListener("click", () => fetchMovies("Ação"));
+  comedyLink.addEventListener("click", () => fetchMovies("Comédia"));
+  animationLink.addEventListener("click", () => fetchMovies("Animação"));
+
+  // Carregue os filmes populares por padrão
+  fetchMovies("Filmes Populares");
+});
